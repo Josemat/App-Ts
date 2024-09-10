9ad4148597a6b2aa7102fc2574b8dd789818361c
josemat <emmatulian@gmail.com>
2024-09-10 15:18:26 -0300

    Actualizacion node + charts

--- vite-project/src/components/BuscarCoche.tsx
+++ vite-project/src/components/BuscarCoche.tsx
@@ -13,6 +13,18 @@ import { useLocation } from 'wouter';
 import { AuthContext } from '../context/setAuth';
 import { CollectionData2, Perfil } from '../vite-env';
 import { Container } from '@mui/material';
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  Legend,
+} from 'recharts';
 
 export default function BuscarCoche() {
   const [coches, setCoches] = React.useState<CollectionData2[]>([]);
@@ -23,6 +35,177 @@ export default function BuscarCoche() {
   const [empleado, setEmpleado] = React.useState<Perfil[]>([]);
   const [busca, setBusca] = React.useState<string>();
   const context = React.useContext(AuthContext);
+
+  const asistenciasAnioPasado = coches.filter(
+    (asis) => Number(dayjs(asis.fecha).format('YYYY')) === 2023
+  );
+  function buscarCocheGrafico2023(empresa: string, mes: number): number {
+    let coche = asistenciasAnioPasado.filter(
+      (asi) =>
+        asi.empresa == empresa && Number(dayjs(asi.fecha).format('MM')) == mes
+    ).length;
+    return coche;
+  }
+  function buscarCocheGrafico(empresa: string, mes: number): number {
+    let coche = coches
+      .filter(
+        (asis) =>
+          Number(dayjs(asis.fecha).format('YYYY')) === Number(dayjs().year())
+      )
+      .filter(
+        (asi) =>
+          asi.empresa == empresa && Number(dayjs(asi.fecha).format('MM')) == mes
+      ).length;
+    return coche;
+  }
+  const data2023 = [
+    {
+      name: 'Enero',
+      Ersa: buscarCocheGrafico2023('ERSA', 0o1),
+      Tamse: buscarCocheGrafico2023('TAMSE', 0o1),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 0o1),
+    },
+    {
+      name: 'Febrero',
+      Ersa: buscarCocheGrafico2023('ERSA', 0o2),
+      Tamse: buscarCocheGrafico2023('TAMSE', 0o2),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 0o2),
+    },
+    {
+      name: 'Marzo',
+      Ersa: buscarCocheGrafico2023('ERSA', 0o3),
+      Tamse: buscarCocheGrafico2023('TAMSE', 0o3),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 0o3),
+    },
+    {
+      name: 'Abril',
+      Ersa: buscarCocheGrafico2023('ERSA', 0o4),
+      Tamse: buscarCocheGrafico2023('TAMSE', 0o4),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 0o4),
+    },
+    {
+      name: 'Mayo',
+      Ersa: buscarCocheGrafico2023('ERSA', 0o5),
+      Tamse: buscarCocheGrafico2023('TAMSE', 0o5),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 0o5),
+    },
+    {
+      name: 'Junio',
+      Ersa: buscarCocheGrafico2023('ERSA', 0o6),
+      Tamse: buscarCocheGrafico2023('TAMSE', 0o6),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 0o6),
+    },
+    {
+      name: 'Julio',
+      Ersa: buscarCocheGrafico2023('ERSA', 0o7),
+      Tamse: buscarCocheGrafico2023('TAMSE', 0o7),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 0o7),
+    },
+    {
+      name: 'Agosto',
+      Ersa: buscarCocheGrafico2023('ERSA', 8),
+      Tamse: buscarCocheGrafico2023('TAMSE', 8),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 8),
+    },
+    {
+      name: 'Septiembre',
+      Ersa: buscarCocheGrafico2023('ERSA', 9),
+      Tamse: buscarCocheGrafico2023('TAMSE', 9),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 9),
+    },
+    {
+      name: 'Octubre',
+      Ersa: buscarCocheGrafico2023('ERSA', 10),
+      Tamse: buscarCocheGrafico2023('TAMSE', 10),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 10),
+    },
+    {
+      name: 'Noviembre',
+      Ersa: buscarCocheGrafico2023('ERSA', 11),
+      Tamse: buscarCocheGrafico2023('TAMSE', 11),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 11),
+    },
+    {
+      name: 'Diciembre',
+      Ersa: buscarCocheGrafico2023('ERSA', 12),
+      Tamse: buscarCocheGrafico2023('TAMSE', 12),
+      Coniferal: buscarCocheGrafico2023('CONIFERAL', 12),
+    },
+  ];
+  const data = [
+    {
+      name: 'Enero',
+      Ersa: buscarCocheGrafico('ERSA', 0o1),
+      Tamse: buscarCocheGrafico('TAMSE', 0o1),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 0o1),
+    },
+    {
+      name: 'Febrero',
+      Ersa: buscarCocheGrafico('ERSA', 0o2),
+      Tamse: buscarCocheGrafico('TAMSE', 0o2),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 0o2),
+    },
+    {
+      name: 'Marzo',
+      Ersa: buscarCocheGrafico('ERSA', 0o3),
+      Tamse: buscarCocheGrafico('TAMSE', 0o3),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 0o3),
+    },
+    {
+      name: 'Abril',
+      Ersa: buscarCocheGrafico('ERSA', 0o4),
+      Tamse: buscarCocheGrafico('TAMSE', 0o4),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 0o4),
+    },
+    {
+      name: 'Mayo',
+      Ersa: buscarCocheGrafico('ERSA', 0o5),
+      Tamse: buscarCocheGrafico('TAMSE', 0o5),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 0o5),
+    },
+    {
+      name: 'Junio',
+      Ersa: buscarCocheGrafico('ERSA', 0o6),
+      Tamse: buscarCocheGrafico('TAMSE', 0o6),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 0o6),
+    },
+    {
+      name: 'Julio',
+      Ersa: buscarCocheGrafico('ERSA', 0o7),
+      Tamse: buscarCocheGrafico('TAMSE', 0o7),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 0o7),
+    },
+    {
+      name: 'Agosto',
+      Ersa: buscarCocheGrafico('ERSA', 8),
+      Tamse: buscarCocheGrafico('TAMSE', 8),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 8),
+    },
+    {
+      name: 'Septiembre',
+      Ersa: buscarCocheGrafico('ERSA', 9),
+      Tamse: buscarCocheGrafico('TAMSE', 9),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 9),
+    },
+    {
+      name: 'Octubre',
+      Ersa: buscarCocheGrafico('ERSA', 10),
+      Tamse: buscarCocheGrafico('TAMSE', 10),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 10),
+    },
+    {
+      name: 'Noviembre',
+      Ersa: buscarCocheGrafico('ERSA', 11),
+      Tamse: buscarCocheGrafico('TAMSE', 11),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 11),
+    },
+    {
+      name: 'Diciembre',
+      Ersa: buscarCocheGrafico('ERSA', 12),
+      Tamse: buscarCocheGrafico('TAMSE', 12),
+      Coniferal: buscarCocheGrafico('CONIFERAL', 12),
+    },
+  ];
 
   if (!context?.user.nombre) navigate('/');
   React.useEffect(() => {
@@ -52,10 +235,56 @@ export default function BuscarCoche() {
   }
   return (
     <>
-      {/* <Container sx={{ backgroundColor: 'green', height: '50vh' }}>
-        {' '}
-        probando la wea cosmica
-      </Container> */}
+      <h2>Asistencias 2023</h2>
+      <Container sx={{ backgroundColor: '', height: '40vh' }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            width={500}
+            height={500}
+            data={data2023}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey={'Ersa'} stroke={'#ff0000'} />
+            <Line type="monotone" dataKey="Tamse" stroke="#00eeff" />
+            <Line type="monotone" dataKey="Coniferal" stroke="#aca139" />
+          </LineChart>
+        </ResponsiveContainer>
+      </Container>
+      <h2>Asistencias 2024</h2>
+      <Container sx={{ backgroundColor: '', height: '40vh' }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            width={500}
+            height={500}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey={'Ersa'} stroke={'#ff0000'} />
+            <Line type="monotone" dataKey="Tamse" stroke="#00eeff" />
+            <Line type="monotone" dataKey="Coniferal" stroke="#aca139" />
+          </LineChart>
+        </ResponsiveContainer>
+      </Container>
       <h2>Búsqueda e historial de asistencias</h2>
       <TableContainer component={Paper}>
         <TextField
@@ -82,27 +311,29 @@ export default function BuscarCoche() {
           </TableHead>
           <TableBody>
             {!busca
-              ? coches.map((row) => (
-                  <TableRow
-                    key={Math.random()}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {row.numCoche}
-                    </TableCell>
-                    <TableCell align="left">{row.descripcion}</TableCell>
-                    <TableCell align="left">{row.empresa}</TableCell>
-                    <TableCell align="right">
-                      {dayjs(row.fecha).format('DD/MM/YY')}
-                    </TableCell>
-                    <TableCell align="center">
-                      {dayjs(row.createdAt).format('DD/MM HH:mm')}
-                    </TableCell>
-                    <TableCell align="right">
-                      {empleado ? empleadoNombre(row.uid) : 'Sin nombre'}
-                    </TableCell>
-                  </TableRow>
-                ))
+              ? coches
+                  .map((row) => (
+                    <TableRow
+                      key={Math.random()}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.numCoche}
+                      </TableCell>
+                      <TableCell align="left">{row.descripcion}</TableCell>
+                      <TableCell align="left">{row.empresa}</TableCell>
+                      <TableCell align="right">
+                        {dayjs(row.fecha).format('DD/MM/YY')}
+                      </TableCell>
+                      <TableCell align="center">
+                        {dayjs(row.createdAt).format('DD/MM HH:mm')}
+                      </TableCell>
+                      <TableCell align="right">
+                        {empleado ? empleadoNombre(row.uid) : 'Sin nombre'}
+                      </TableCell>
+                    </TableRow>
+                  ))
+                  .slice(0, 50)
               : cocheBuscador.map((row) => (
                   <TableRow
                     key={Math.random()}
